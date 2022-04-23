fb6c889dc8188cd189d9ac33e68b1733b767ef92
Kedarling <102572716+Kedarling@users.noreply.github.com>
2022-04-23 23:36:04 +0530

    Add files via upload

---
+++ 4_TestPlantAndOutput/output.h
@@ -0,0 +1,10 @@
+pattern are outputs
+And for testing SW(Switch is the input)
+Test plan
+1. MCU is programmed
+2. Check the connection
+3. Switch on the 3.3v  power supply
+4. Check the sequences
+5. Check the time delay
+6. Check the LED brightness
+7. Auto turn off, after the sequence.
